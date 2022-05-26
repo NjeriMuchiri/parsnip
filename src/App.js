afa5191f36b3813f14b792662dc60c358e721371
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-26 21:39:08 +0300

    Added App page contents

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React, {Component} from 'react';
+import TasksPage from './components/TasksPage';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+const mockTasks = [
+  {
+
+    id: 1,
+    title:'Learn Redux',
+    description: 'The store, actions, and reducers, oh my!',
+    status: 'In Progress',
+
+  },
+  {
+
+    id: 2,
+    title: 'Peace on Earth',
+    description: 'No big deal.',
+    status: 'In progress',
+
+  },
+];
+class App extends Component {
+  render(){
+    return (
+      <div className='main-content'>
+        <TasksPage  tasks={mockTasks} />
+      </div>
+    )
+  }
 }
 
 export default App;
